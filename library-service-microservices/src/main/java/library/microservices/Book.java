91df5a532caf8aecb923225d4bb488f55fcc1c94
davide.pravata <davide.pravata@docomodigital.com>
2018-12-06 15:33:44 +0100

    Ongoing

--- library-service-microservices/src/main/java/library/microservices/Book.java
+++ library-service-microservices/src/main/java/library/microservices/Book.java
@@ -1,17 +1,36 @@
 package library.microservices;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+/*
+create table books(
+id bigint(20) NOT NULL AUTO_INCREMENT,
+title varchar(255) NOT NULL,
+cost decimal(10,2) NOT NULL,
+isbn char(13) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY(isbn)
+) */
 @Entity
 public class Book {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name="title")
+    @NotNull(message = "Invalid empty title")
     private String title;
+    @Column(name="cost")
+    @Pattern(regexp = "[0-9]+(.[0-9]{1,2})?", message = "Invalid price")
     private String cost;
+    @NotNull(message="Invalid empty price")
+    @Size(min=13,max=13, message="Size not valid for isbn, must be 13 chars")
+    @Column(name="isbn")
     private String isbn;
 
-
-    public Book(long id, String title, String cost, String isbn) {
+    public Book(long id, @NotNull(message = "Invalid empty title") String title, @Pattern(regexp = "[0-9]+(.[0-9]{1,2})?", message = "Invalid price") String cost, @NotNull(message = "Invalid empty price") @Size(min = 13, max = 13, message = "Size not valid for isbn, must be 13 chars") String isbn) {
         this.id = id;
         this.title = title;
         this.cost = cost;
