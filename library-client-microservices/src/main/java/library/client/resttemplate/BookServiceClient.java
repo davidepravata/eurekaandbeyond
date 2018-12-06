5e8a5e0c7e026b2357a8bf66f87121400faec9d5
davide.pravata <davide.pravata@docomodigital.com>
2018-12-06 12:25:54 +0100

    Ongoing

---
+++ library-client-microservices/src/main/java/library/client/resttemplate/BookServiceClient.java
@@ -0,0 +1,27 @@
+package library.client.resttemplate;
+
+import library.microservices.entities.Book;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
+public class BookServiceClient {
+
+    private RestTemplate rest;
+
+    public BookServiceClient(@LoadBalanced RestTemplate rest) {
+        this.rest = rest;
+    }
+
+    public Book getBookById(String bookId) {
+        return rest.getForObject("http://library-service/book/{id}", Book.class, bookId);
+    }
+
+    public Iterable<Book> getAllBooks() {
+        Book[] books = rest.getForObject("http://library-service/ingredients", Book[].class);
+        return Arrays.asList(books);
+    }
+}
---
+++ library-client-microservices/src/main/java/library/controllers/BookController.java
@@ -0,0 +1,36 @@
+package library.controllers;
+
+import library.LibraryClientApplication;
+import library.client.resttemplate.BookServiceClient;
+import library.microservices.entities.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books_list")
+public class BookController {
+
+    private BookServiceClient bookServiceClient;
+    @Autowired
+    public BookController(BookServiceClient bookServiceClient) {
+        this.bookServiceClient = bookServiceClient;
+    }
+
+    @GetMapping
+    public String booksList(Model model) {
+        model.addAttribute("books", bookServiceClient.getAllBooks());
+        return "bookList";
+    }
+
+    @GetMapping("/{id}")
+    public String bookDetailPage(@PathVariable("id") String id, Model model) {
+        model.addAttribute("book", bookServiceClient.getBookById(id));
+        return "bookDetail";
+    }
+}
